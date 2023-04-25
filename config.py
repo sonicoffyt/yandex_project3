cedbb5a2622c6f033682a98ef0deb8a9d9c454ed
sonicoffyt <72821480+sonicoffyt@users.noreply.github.com>
2023-04-25 11:51:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 
-TOKEN = '6289566725:AAG4dBOIohXpBuhXRO8IkS5ROEyII39g290'
+TOKEN = ''
 PATH_TO_AI_TOKENS = r'ai_tokens.txt'
 PATH_TO_INVALID_AI_TOKENS = r'invalid_ai_tokens.txt'
-ADMIN_IDS = (5617149700, )
-BOT_USERNAME = 'chat_gpt_projectBot'
+ADMIN_IDS = ()
+BOT_USERNAME = ''
 BANNED_USERS = []
 bot = Bot(TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
